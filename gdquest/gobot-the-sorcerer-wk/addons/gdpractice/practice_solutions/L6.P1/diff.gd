36323cc18173a9e84f163864f820c734dc685c51
MohamedShah H <iguessfive@outlook.com>
2025-12-31 10:02:10 +0000

    removed addons from repo

--- gdquest/gobot-the-sorcerer-wk/addons/gdpractice/practice_solutions/L6.P1/diff.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-static func bullet(scene_root: Node) -> void:
-	scene_root.bullet_skin_scene = null
--- gdquest/gobot-the-sorcerer-wk/addons/gdpractice/practice_solutions/L7.P1/diff.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-static func machine_gun(scene_root: Node) -> void:
-	scene_root.bullet_scene = null
